0ac6486d652c15187f5ad2e3890a661098919dc8
gooldeer <moysa.sergey@gmail.com>
2019-08-05 14:55:57 +0300

    redesigned signing and all games emtpy view

--- auth/src/main/java/com/firebase/ui/auth/ui/idp/AuthMethodPickerActivity.java
+++ auth/src/main/java/com/firebase/ui/auth/ui/idp/AuthMethodPickerActivity.java
@@ -19,7 +19,11 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.*;
+import android.support.annotation.IdRes;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RestrictTo;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
 import android.view.View;
@@ -29,8 +33,14 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.firebase.ui.auth.*;
+import com.firebase.ui.auth.AuthMethodPickerLayout;
+import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.AuthUI.IdpConfig;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.FirebaseAuthAnonymousUpgradeException;
+import com.firebase.ui.auth.FirebaseUiException;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.R;
 import com.firebase.ui.auth.data.model.FlowParameters;
 import com.firebase.ui.auth.data.model.LinkedInAuthProvider;
 import com.firebase.ui.auth.data.model.UserCancellationException;
@@ -118,6 +128,10 @@ public class AuthMethodPickerActivity extends AppCompatBase {
                 ImageView logo = findViewById(R.id.logo);
                 logo.setImageResource(logoId);
             }
+        }
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
         }
 
         boolean tosAndPpConfigured = getFlowParams().isPrivacyPolicyUrlProvided()
